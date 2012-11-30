9869895a72e57c2cc0c689bad950bae8a2705c39
Paul Ivanov <pivanov314@gmail.com>
2012-11-30 10:29:06 -0800

    tried adding fileinput, doesn't quite work

--- nbcat.py
+++ nbcat.py
@@ -9,6 +9,7 @@ python nbmerge.py A.ipynb B.ipynb C.ipynb > merged.ipynb
 import io
 import os
 import sys
+import fileinput
 
 from IPython.nbformat import current
 
@@ -20,9 +21,15 @@ def merge_notebooks(filenames):
         if merged is None:
             merged = nb
         else:
+            # TODO: add an optional marker between joined notebooks
+            # like an horizontal rule, for example, or some other arbitrary
+            # (user specified) markdown cell)
             merged.worksheets[0].cells.extend(nb.worksheets[0].cells)
     merged.metadata.name += "_merged"
     print current.writes(merged, 'json')
 
 if __name__ == '__main__':
+    #files = [x[:-1] for x in fileinput.input()]
+    # reading from stdin (needs modification)
     merge_notebooks(sys.argv[1:])
+
