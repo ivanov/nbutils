22ec9e1ba852b20e19ce0cee71673f5116ceb14a
Paul Ivanov <pivanov314@gmail.com>
2012-11-30 11:52:39 -0800

    documented and refactored nbexpload a bit

--- nbexplode.py
+++ nbexplode.py
@@ -1,194 +1,80 @@
 #!/usr/bin/env python
 # from https://gist.github.com/2620735
 """
-simple example script for running and testing notebooks.
+Explode a parametrized notebook into as many notebooks as there are
+combinations of parameters.
 
-Usage: `ipnbdoctest.py foo.ipynb [bar.ipynb [...]]`
+Takes a notebook where the first cell looks something like this::
+    
+    ## Parameters
+    x = [ 1, 5, 10, 20 ]
+    y = 'I love python'.split()
 
-Each cell is submitted to the kernel, and the outputs are compared with those stored in the notebook.
+and creates 12 notebooks, with the first cell replaced by all combinations of
+members of `x` and `y`, for example, the first notebook that will get exploded
+will have this as it's first cell::
+
+    ## Parameterized by sample.ipynb
+    x = 1
+    y = 'I'
+
+and the last exploded notebook will have its first cell be::
+
+    ## Parameterized by sample.ipynb
+    x = 20 
+    y = 'python'
 """
 
-import os,sys,time
-import base64
-import re
-
-from collections import defaultdict
-from Queue import Empty
-
-from IPython.zmq.blockingkernelmanager import BlockingKernelManager
-from IPython.nbformat.current import reads, writes, NotebookNode
-
+import os
 import logging
+import itertools
+from collections import OrderedDict
+from IPython.nbformat.current import reads, writes
+
 logging.basicConfig(format='%(message)s')
 log = logging.getLogger(os.path.basename(__file__))
 
-def compare_png(a64, b64):
-    """compare two b64 PNGs (incomplete)"""
-    try:
-        import Image
-    except ImportError:
-        pass
-    adata = base64.decodestring(a64)
-    bdata = base64.decodestring(b64)
-    return True
-
-def sanitize(s):
-    """sanitize a string for comparison.
-    
-    fix universal newlines, strip trailing newlines, and normalize likely random values (memory addresses and UUIDs)
+def explode(nb, quiet=False, stdout=False):
     """
-    # normalize newline:
-    s = s.replace('\r\n', '\n')
-    
-    # ignore trailing newlines (but not space)
-    s = s.rstrip('\n')
-    
-    # normalize hex addresses:
-    s = re.sub(r'0x[a-f0-9]+', '0xFFFFFFFF', s)
-    
-    # normalize UUIDs:
-    s = re.sub(r'[a-f0-9]{8}(\-[a-f0-9]{4}){3}\-[a-f0-9]{12}', 'U-U-I-D', s)
-    
-    return s
+    Explode a parametrized notebook
 
-
-def consolidate_outputs(outputs):
-    """consolidate outputs into a summary dict (incomplete)"""
-    data = defaultdict(list)
-    data['stdout'] = ''
-    data['stderr'] = ''
+    Parameters
+    ----------
+    nb : IPython Notebook object
+        The parameterized notebook to be exploded
+    quiet : bool (default=False)
+        whether to print out filenames of the exploded notebooks
     
-    for out in outputs:
-        if out.type == 'stream':
-            data[out.stream] += out.text
-        elif out.type == 'pyerr':
-            data['pyerr'] = dict(ename=out.ename, evalue=out.evalue)
-        else:
-            for key in ('png', 'svg', 'latex', 'html', 'javascript', 'text', 'jpeg',):
-                if key in out:
-                    data[key].append(out[key])
-    return data
-
-
-def compare_outputs(test, ref, skip_compare=('png', 'traceback', 'latex', 'prompt_number')):
-    for key in ref:
-        if key not in test:
-            log.info("missing key: %s != %s" % (test.keys(), ref.keys()))
-            return False
-        elif key not in skip_compare and sanitize(test[key]) != sanitize(ref[key]):
-            log.info("mismatch %s:" % key)
-            log.info(test[key])
-            log.info('  !=  ')
-            log.info(ref[key])
-            return False
-    return True
-
-
-def run_cell(km, cell):
-    shell = km.shell_channel
-    iopub = km.sub_channel
-    # print "\n\ntesting:"
-    # print cell.input
-    shell.execute(cell.input)
-    # wait for finish, maximum 20s
-    shell.get_msg(timeout=20)
-    outs = []
     
-    while True:
-        try:
-            msg = iopub.get_msg(timeout=0.2)
-        except Empty:
-            break
-        msg_type = msg['msg_type']
-        if msg_type in ('status', 'pyin'):
-            continue
-        elif msg_type == 'clear_output':
-            outs = []
-            continue
-        
-        content = msg['content']
-        # print msg_type, content
-        out = NotebookNode(output_type=msg_type)
-        
-        if msg_type == 'stream':
-            out.stream = content['name']
-            out.text = content['data']
-        elif msg_type in ('display_data', 'pyout'):
-            for mime, data in content['data'].iteritems():
-                attr = mime.split('/')[-1].lower()
-                # this gets most right, but fix svg+html, plain
-                attr = attr.replace('+xml', '').replace('plain', 'text')
-                setattr(out, attr, data)
-            if msg_type == 'pyout':
-                out.prompt_number = content['execution_count']
-        elif msg_type == 'pyerr':
-            out.ename = content['ename']
-            out.evalue = content['evalue']
-            out.traceback = content['traceback']
-        else:
-            print "unhandled iopub msg:", msg_type
-        
-        outs.append(out)
-    return outs
-    
+    """
+    first_cell = nb.worksheets[0].cells[0]
+    if not first_cell.input.lower().startswith("## parameters"):
+        log.warning("no parameters found in this notebook")
+        return
+    params = OrderedDict()
+    exec(first_cell.input, globals(), params)
+    for i, p in enumerate(itertools.product(*params.values())):
+        log.info("p = %s", p)
+        header = "## Parameterized by %s\n" % ipynb
+        assignments = []
+        for k,v in zip(params.keys(), p):
+            assignments.append('%s = %s #damnit' % (k,repr(v)))
+        first_cell.input = header + "\n".join(assignments)
 
-def test_notebook(nb):
-    km = BlockingKernelManager()
-    km.start_kernel(extra_arguments=['--pylab=inline'], stderr=open(os.devnull, 'w'))
-    km.start_channels()
-    # run %pylab inline, because some notebooks assume this
-    # even though they shouldn't
-    km.shell_channel.execute("pass")
-    km.shell_channel.get_msg()
-    while True:
-        try:
-            km.sub_channel.get_msg(timeout=1)
-        except Empty:
-            break
-    
-    successes = 0
-    failures = 0
-    errors = 0
-    for ws in nb.worksheets:
-        for cell in ws.cells:
-            if cell.cell_type != 'code':
-                continue
-            try:
-                outs = run_cell(km, cell)
-            except Exception as e:
-                print "failed to run cell:", repr(e)
-                print cell.input
-                errors += 1
-                continue
-            
-            failed = False
-            for out, ref in zip(outs, cell.outputs):
-                if not compare_outputs(out, ref):
-                    failed = True
-            if failed:
-                failures += 1
-            else:
-                successes += 1
-            cell.outputs = outs
-            if log.getEffectiveLevel() < logging.FATAL:
-                sys.stderr.write('.')
-
-    if log.getEffectiveLevel() <= logging.WARNING:
-        sys.stderr.write('\n')
-    log.info("tested notebook %s" % nb.metadata.name)
-    log.info("    %3i cells successfully replicated" % successes)
-    if failures:
-        log.info("    %3i cells mismatched output" % failures)
-    if errors:
-        log.info("    %3i cells failed to complete" % errors)
-    km.shutdown_kernel()
-    del km
-    return nb
+        nb_as_json = writes(nb, 'json')
+        basename = ipynb.rsplit('.ipynb')[0]
+        outfile = args.prefix + basename +"%04d.ipynb" % i
+        with open(outfile, 'w') as f:
+            f.write(nb_as_json)
+        log.info('writing file...')
+        if not quiet:
+            print outfile
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
+    from argparse import ArgumentParser, RawTextHelpFormatter
 
-    parser = ArgumentParser(description=__doc__)
+    parser = ArgumentParser(description=__doc__,
+            formatter_class=RawTextHelpFormatter)
     parser.add_argument('inputs', nargs='+', metavar='input',
                         help='Paths to notebook files.')
     parser.add_argument('-i', '--inplace', '--in-place', default=False,
@@ -196,11 +82,11 @@ if __name__ == '__main__':
             help='Overwrite existing notebook when given.')
 
     parser.add_argument('-p', '--prefix', default='',
-                        help='Path for the destination of converted output')
+                        help='prefix for the resulting filenames')
     parser.add_argument('-v', '--verbose', default=False, action='store_true',
-                        help='be verbose about cells comparisons')
+                        help='Be verbose')
     parser.add_argument('-q', '--quiet', default=False, action='store_true',
-                        help='be verbose about cells comparisons')
+                        help='Be quiet, or I shall taunt you a second time!')
     parser.add_argument('-O', '--stdout', default=False, action='store_true',
         help='Print converted output instead of sending it to a file')
 
@@ -213,27 +99,8 @@ if __name__ == '__main__':
 
 
     for ipynb in args.inputs:
-        log.warning('Running '+ ipynb)
+        log.info('Exploding '+ ipynb)
         with open(ipynb) as f:
             nb = reads(f.read(), 'json')
 
-        first_cell = nb.worksheets[0].cells[0]
-        if first_cell.input.startswith("## parameters"):
-            from collections import OrderedDict
-            params = OrderedDict()
-            exec(first_cell.input, globals(), params)
-            import itertools
-            for i, p in enumerate(itertools.product(*params.values())):
-                log.info("p = ", p)
-                header = "## Parameterized by %s\n" % ipynb
-                assignments = []
-                for k,v in zip(params.keys(), p):
-                    assignments.append('%s = %s #damnit' % (k,repr(v)))
-                first_cell.input = header + "\n".join(assignments)
-
-                nb_as_json = writes(nb, 'json')
-                basename = ipynb.rsplit('.ipynb')[0]
-                outfile = args.prefix + basename +"%04d.ipynb" % i
-                with open(outfile, 'w') as f:
-                    f.write(nb_as_json)
-                log.warning("Wrote file " + outfile)
+        explode(nb, quiet=args.quiet, stdout=args.stdout)
